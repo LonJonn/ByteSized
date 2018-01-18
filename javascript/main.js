4f27cd5c77a1bca15223647c53605177418d526a
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-01-19 02:02:22 +1100

    t

--- javascript/main.js
+++ javascript/main.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         bits += GenRate;
     }
 
-//########################
+//#######################
 
     $("html").click(function () {
         $(".program").animate({
