27b2f33cdfdf64a001ad81487eb82157aa75d3a1
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-01-19 01:58:10 +1100

    added some main coode

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,26 @@
 var colourList = ['#ff8787', '#f783ac', '#da77f2', '#748ffc', '#3bc9db', '#69db7c', '#ffa94d'];
+var bits = 0;
+var clickRate = 1;
+var GenRate = 0;
 
 $(document).ready(function () {
+
+    function updateBits() {
+        $("#bits").html(bits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+    }
+
+    function bitGenerator() {
+        $("#bitGenRate").html(GenRate);
+        bits += GenRate;
+    }
+
+//########################
 
     $("html").click(function () {
         $(".program").animate({
             bottom: '100px',
             opacity: '1'
         }, 1500);
-        //$(".program").fadeIn(1500);
     })
 
     $('.header h2').on('click', function () {
@@ -17,9 +30,18 @@ $(document).ready(function () {
     })
 
     $('.button').on('click', function () {
-        $(this).css('background', colourList[Math.floor(Math.random() * colourList.length)])
+        $(this).css('background', colourList[Math.floor(Math.random() * colourList.length)]);
+        bits += clickRate;
+        $("#bits").html(bits.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
+        $(".fa-microchip").css('transform', 'rotate(' + Math.floor(Math.random() * 180) + 'deg)');
     }).on('mouseleave', function () {
         $(this).css('background', '#67bcff');
+        $(".fa-microchip").css('transform', 'rotate(0)');
     })
+
+//#########################
+
+    setInterval(updateBits, 250);
+    setInterval(bitGenerator, 1000)
 
 })
