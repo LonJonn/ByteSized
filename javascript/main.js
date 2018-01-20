a85de547309e7abfe81b28ba57e40b4fdb26278e
Leon Salsiccia <leon.salsiccia@gmail.com>
2018-01-20 19:40:10 +1100

    finish login, fixed icons on login

--- javascript/main.js
+++ javascript/main.js
@@ -5,12 +5,48 @@ var genRate = parseInt(localStorage.getItem('Generator Rate'));
 
 $(document).ready(function () {
 
+    if (localStorage.getItem('Users') === null) {
+        localStorage.setItem('Users', 'Admin')
+        localStorage.setItem('Admin', '1234:999999:10000:999')
+    }
+
+ //START FUNCTIONS#######################################################
+
     function save() {
         localStorage.setItem('Bits Stored', bits);
         localStorage.setItem('Click Rate', clickRate);
         localStorage.setItem('Generator Rate', genRate);
         console.log("saved at: " + new Date().toLocaleString());
     }
+
+//LOGIN AND REGISTER#####################################################
+
+    function checkPass() {
+        var currentUser = $('#username').val();
+        var currentUserAcc = localStorage.getItem(currentUser).split(':');
+        if (currentUserAcc.includes($('#password').val())) {
+            alert('Welcome.');
+        } else {
+            alert("Inccorect Password.");
+        }
+    }
+
+    function register() {
+        var temp = $('#username').val()
+        if ($('#username').val() !== '' && localStorage.getItem('Users').split(':').includes(temp) === false) {
+            if ($('#password').val() !== '') {
+                localStorage.setItem('Users', localStorage.getItem('Users') + ":" + $('#username').val());
+                localStorage.setItem($('#username').val(), $('#password').val() + ':0:1:0');
+                alert('Registration Complete!');
+            } else {
+                alert('Please enter a password!');
+            }
+        } else {
+            alert('Username already used!');
+        }
+    }
+
+//LOGIN AND REGISTER#####################################################
 
     function updateBits() {
         $("#bits").html(Math.round(bits).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
@@ -21,7 +57,7 @@ $(document).ready(function () {
         bits += genRate / 4;
     }
 
-    //#######################
+    //END FUNCTIONS######################################################
 
     /*$("html").click(function () {
         $(".program").animate({
@@ -36,12 +72,24 @@ $(document).ready(function () {
         $(this).css('letter-spacing', '3px');
     })
 
-    $('#showhide').on('click', function() {
+    $('#showhide').on('click', function () {
         if ($('#password').attr('type') === 'text') {
             $('#password').attr('type', 'password')
         } else {
             $('#password').attr('type', 'text')
         }
+    })
+
+    $('#submit').on('click', function () {
+        if (localStorage.getItem('Users').split(':').includes($('#username').val())) {
+            checkPass();
+        } else {
+            alert("Username doesn't exist!");
+        }
+    })
+
+    $('#register').on('click', function () {
+        register();
     })
 
     $('.button').on('click', function () {
@@ -54,9 +102,20 @@ $(document).ready(function () {
         $(".fa-microchip").css('transform', 'rotate(0)');
     })
 
+
+
+
+
     //#########################
 
     setInterval(save, 120000);
+    setInterval(function () {
+        if (localStorage.getItem('Users').split(':').includes($('#username').val())) {
+            $('#username').css('border', 'solid 2px pink');
+        } else {
+            $('#username').css('border', 'solid 2px #e9ecef')
+        }
+    }, 1000)
     //setInterval(updateBits, 250);
     //setInterval(bitGenerator, 250)
 
